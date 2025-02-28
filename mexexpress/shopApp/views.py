ef7533b0d841aeb7c1e6072cbbf5937bd7416b4f
Alex Rodriguez <alejandro.rodriguez112m@gmail.com>
2025-02-28 12:56:51 -0600

    Se agregaron imagenes tanto en views como en el index

--- mexexpress/shopApp/views.py
+++ mexexpress/shopApp/views.py
@@ -6,19 +6,23 @@ def index(request):
     productos = [
         { 
          'name' : 'Relog Citizen',
-         'cost' : 150.00
+         'cost' : 150.00,
+         'image': 'https://ss376.liverpool.com.mx/xl/1136440248.jpg'
          },
          {
              'name' : 'Peluche de Batman',
-             'cost' : 35.00
+             'cost' : 35.00,
+             'image': 'https://m.media-amazon.com/images/I/81eU3-uPjWL.jpg'
          },
          {
              'name' : 'Consola de videojuegos Xbox',
-             'cost' : 500.00
+             'cost' : 500.00,
+             'image': 'https://http2.mlstatic.com/D_Q_NP_942133-MLA74651936102_022024-O.webp'
          },
          {
              'name' : 'RTX 3060',
-             'cost' : 350.00
+             'cost' : 350.00,
+             'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuwkXO4SLaQaDrbkzzTLErafkmjAPk8fJpIw&s'
          }
     ]
     context = {
