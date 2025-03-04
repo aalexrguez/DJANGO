f9c78dbcf4a0fdcce538de0b90c58df329de0561
Alex Rodriguez <alejandro.rodriguez112m@gmail.com>
2025-03-04 14:48:51 -0600

    Se agregan los modelos de producto y historial

--- mexexpress/shopApp/models.py
+++ mexexpress/shopApp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    product_name = models.CharField(max_length=100,unique=True)
+    product_description = models.CharField(max_length=255)
+    product_full_cost = models.DecimalField(max_digits=8,decimal_places=2)
+    product_is_offer = models.BooleanField()
+    product_offer_cost = models.DecimalField(max_digits=8,decimal_places=2)
+
+    def __str__(self):
+        return self.product_name
+
+class HistoricalCost(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    chance_date = models.DateTimeField()
+    product_cost = models.DecimalField(max_digits=8,decimal_places=2)
+    product_is_offer = models.BooleanField()
+
+    def __str__(self):
+        return self.product.product_name + " " + self.chance_date.strftime('%Y-$m-%d %H:%M')
