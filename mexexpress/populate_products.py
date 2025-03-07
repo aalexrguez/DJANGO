8fb9ed93412926df0487f7661a9f50c1af75bc62
Alex Rodriguez <alejandro.rodriguez112m@gmail.com>
2025-03-07 15:23:40 -0600

    Se agrega la funcion para poblar la base de datos y se agregan los datos

--- mexexpress/populate_products.py
+++ mexexpress/populate_products.py
@@ -1,5 +1,5 @@
 import os
-os.environ.setdefault('DJANGO_SETTING_MODULE','mexexpress.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','mexexpress.settings')
 import django
 django.setup()
 
@@ -13,7 +13,24 @@ from shopApp.models import Product
 
 faker_generator = Faker() # se crear el objeto faker
 
+def populate_products(n_products=5):
+
+    for i in range(n_products):
+        fake_name = faker_generator.sentence(nb_words=6,variable_nb_words=True)
+        fake_description = faker_generator.paragraph(nb_sentences=3,variable_nb_sentences=True)
+        fake_full_cost = random.uniform(10,14000)
+        fake_is_offer = random.random() > 0.5
+        fake_offer_cost = random.uniform(5,fake_full_cost) if fake_is_offer else fake_full_cost
+
+        product = Product.objects.get_or_create(
+            product_name = fake_name,
+            product_description = fake_description,
+            product_full_cost = fake_full_cost,
+            product_is_offer = fake_is_offer,
+            product_offer_cost = fake_offer_cost
+        )
+
 if __name__ == '__main__':
     print('Empezar a poblar la base de datos')
-
+    populate_products(20)
     print('finalizado')
