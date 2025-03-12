016afc9d9ac5db66ff4ca859de579d58b15db056
Alex Rodriguez <alejandro.rodriguez112m@gmail.com>
2025-03-11 20:12:50 -0600

    En el template about se le agrega la tabla de contacts

--- mexexpress/shopApp/admin.py
+++ mexexpress/shopApp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Product,HistoricalCost
+from .models import Product,HistoricalCost,Contacts
 # Register your models here.
 
 admin.site.register(Product)
 admin.site.register(HistoricalCost)
+admin.site.register(Contacts)
