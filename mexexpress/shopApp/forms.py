e23cc83639e6f481903cd691d005ae5e1cd826d7
Alex Rodriguez <alejandro.rodriguez112m@gmail.com>
2025-03-29 12:23:35 -0600

    Se crea la clase FormContact

--- mexexpress/shopApp/forms.py
+++ mexexpress/shopApp/forms.py
@@ -4,3 +4,13 @@ class FormComment(forms.Form):
     full_name = forms.CharField()
     email = forms.EmailField()
     comment = forms.CharField(widget=forms.Textarea)
+
+class FormContact(forms.Form):
+    full_name = forms.CharField(label="Nombre ",max_length=255,required=True)
+    address = forms.CharField(label="direccion ",max_length=255,required=True)
+    phone = forms.CharField(label="Telefono ",max_length=15,required=True)
+    email = forms.EmailField(label="Email ",max_length=155,required=True)
+    activate = forms.BooleanField(required=True)
+
+
+
