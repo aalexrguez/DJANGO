37ae54db88a2e7a173f35fae3965b9546d791019
Alex Rodriguez <alejandro.rodriguez112m@gmail.com>
2025-03-29 12:24:26 -0600

    Se agrego la url de form_contact

--- mexexpress/shopApp/urls.py
+++ mexexpress/shopApp/urls.py
@@ -6,5 +6,6 @@ app_name = 'app'
 urlpatterns = [
     path('',views.index,name='index'),
     path('about/',views.about,name='about'),
-    path('form_comment/',views.form_comment,name='form_comment')
+    path('form_comment/',views.form_comment,name='form_comment'),
+    path('form_contact/',views.form_contact,name='form_contact')
 ]
