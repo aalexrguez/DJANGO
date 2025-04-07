5d0644b4e52f18ec9dbf9cd67b3148dd2cc07e86
Alex Rodriguez <alejandro.rodriguez112m@gmail.com>
2025-04-07 14:01:40 -0600

    Se agregaron cambios al formComment

--- mexexpress/shopApp/forms.py
+++ mexexpress/shopApp/forms.py
@@ -1,9 +1,14 @@
 from django import forms
+from django.core import validators
 
+
+def check_for_z(value):
+    if value[0].lower() != 'z':
+        raise forms.ValidationError('no empieza con z')
 class FormComment(forms.Form):
-    full_name = forms.CharField(label='Nombre')
+    full_name = forms.CharField(label='Nombre',validators=[validators.MaxLengthValidator(20),check_for_z])
     email = forms.EmailField(label='Email')
-    comment = forms.CharField(label='Comentario',widget=forms.Textarea)
+    comment = forms.CharField(label='Comentario',widget=forms.Textarea,validators=[validators.MaxLengthValidator(150)])
 
 class FormContact(forms.Form):
     full_name = forms.CharField(label="Nombre ",max_length=255,required=True)
